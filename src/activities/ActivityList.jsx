5e862ae2197213a5aa6ff18c955b4f2c85dc7534
Aiden Graham <aiden0405@gmail.com>
2025-10-16 20:13:01 -0500

    finished assignment

--- src/activities/ActivityList.jsx
+++ src/activities/ActivityList.jsx
@@ -1,26 +1,7 @@
 import { useState, useEffect } from "react";
 import { getActivities } from "../api/activities";
 import { useAuth } from "../auth/AuthContext";
-
-export async function DeleteActivity(id, token) {
-  if (!token) {
-    console.warn("No token found. User not logged in.");
-    return;
-  }
-
-  try {
-    const response = await fetch(`/api/activities/${id}`, {
-      method: "DELETE",
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-    if (!response.ok)
-      throw Error("Failed to delete activity. Is it deleted already?");
-  } catch (error) {
-    console.error(error);
-  }
-}
+import { deleteActivity } from "../api/activities";
 
 export default function ActivityList() {
   const { token } = useAuth();
@@ -35,13 +16,28 @@ export default function ActivityList() {
     syncActivities();
   }, []);
 
+  const handleDelete = async (id) => {
+    try {
+      await deleteActivity(token, id);
+      syncActivities();
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
   return (
     <ul>
       {activities.map((activity) => (
         <li key={activity.id}>
           {activity.name}
           {token && (
-            <button action={DeleteActivity(activity.id, token)}>Delete</button>
+            <button
+              onClick={() => {
+                handleDelete(activity.id);
+              }}
+            >
+              Delete
+            </button>
           )}
         </li>
       ))}
